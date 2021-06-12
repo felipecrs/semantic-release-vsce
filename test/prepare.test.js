db9132d0e639727ac7c4f3a84c7e65e4483949c9
Felipe Santos <felipecassiors@gmail.com>
2021-06-12 17:58:22 -0300

    feat: use vsce to bump version
    As this feature was introduced in vsce@1.93.0 through
    https://github.com/microsoft/vscode-vsce/pull/555.

--- test/prepare.test.js
+++ test/prepare.test.js
@@ -8,82 +8,75 @@ const logger = {
 
 test.beforeEach(t => {
   t.context.stubs = {
-    execaStub: sinon.stub(),
-    updatePackageVersionStub: sinon.stub().resolves()
+    execaStub: sinon.stub()
   };
 });
 
 test.afterEach(t => {
   t.context.stubs.execaStub.resetHistory();
-  t.context.stubs.updatePackageVersionStub.resetHistory();
 });
 
 test('packageVsix is not specified', async t => {
-  const { updatePackageVersionStub, execaStub } = t.context.stubs;
+  const { execaStub } = t.context.stubs;
   const prepare = proxyquire('../lib/prepare', {
-    execa: execaStub,
-    './update-package-version': updatePackageVersionStub
+    execa: execaStub
   });
 
-  await prepare('1.0.0', undefined, undefined, logger);
+  const version = '1.0.0';
+  await prepare(version, undefined, undefined, logger);
 
-  t.true(updatePackageVersionStub.calledOnce);
   t.true(execaStub.notCalled);
 });
 
 test('packageVsix is not specified but yarn is true', async t => {
-  const { updatePackageVersionStub, execaStub } = t.context.stubs;
+  const { execaStub } = t.context.stubs;
   const prepare = proxyquire('../lib/prepare', {
-    execa: execaStub,
-    './update-package-version': updatePackageVersionStub
+    execa: execaStub
   });
 
+  const version = '1.0.0';
   const yarn = true;
-  await prepare('1.0.0', undefined, yarn, logger);
+  await prepare(version, undefined, yarn, logger);
 
-  t.true(updatePackageVersionStub.calledOnce);
   t.true(execaStub.notCalled);
 });
 
 test('packageVsix is a string', async t => {
-  const { updatePackageVersionStub, execaStub } = t.context.stubs;
+  const { execaStub } = t.context.stubs;
   const prepare = proxyquire('../lib/prepare', {
-    execa: execaStub,
-    './update-package-version': updatePackageVersionStub
+    execa: execaStub
   });
 
+  const version = '1.0.0';
   const packageVsix = 'test.vsix';
-  await prepare('1.0.0', packageVsix, undefined, logger);
+  await prepare(version, packageVsix, undefined, logger);
 
-  t.true(updatePackageVersionStub.calledOnce);
-  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['package', '--out', packageVsix], { stdio: 'inherit' }]);
+  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['package', version, '--no-git-tag-version', '--out', packageVsix], { stdio: 'inherit' }]);
 });
 
 test('packageVsix is true', async t => {
-  const { updatePackageVersionStub, execaStub } = t.context.stubs;
+  const { execaStub } = t.context.stubs;
   const prepare = proxyquire('../lib/prepare', {
-    execa: execaStub,
-    './update-package-version': updatePackageVersionStub
+    execa: execaStub
   });
 
+  const version = '1.0.0';
   const packageVsix = true;
-  await prepare('1.0.0', packageVsix, undefined, logger);
+  await prepare(version, packageVsix, undefined, logger);
 
-  t.true(updatePackageVersionStub.calledOnce);
-  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['package'], { stdio: 'inherit' }]);
+  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['package', version, '--no-git-tag-version'], { stdio: 'inherit' }]);
 });
 
 test('packageVsix is true and yarn is true', async t => {
-  const { updatePackageVersionStub, execaStub } = t.context.stubs;
+  const { execaStub } = t.context.stubs;
   const prepare = proxyquire('../lib/prepare', {
-    execa: execaStub,
-    './update-package-version': updatePackageVersionStub
+    execa: execaStub
   });
 
+  const version = '1.0.0';
   const packageVsix = true;
   const yarn = true;
-  await prepare('1.0.0', packageVsix, yarn, logger);
+  await prepare(version, packageVsix, yarn, logger);
 
-  t.true(updatePackageVersionStub.calledOnce);
-  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['package', '--yarn'], { stdio: 'inherit' }]);
+  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['package', version, '--no-git-tag-version', '--yarn'], { stdio: 'inherit' }]);
 });
