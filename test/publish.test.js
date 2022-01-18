3ae8a575ed4ceb976414849aebf1960d2dab86e9
Felipe Santos <felipecassiors@gmail.com>
2022-01-18 08:56:40 -0300

    refactor!: drop `yarn` option (#233)
    As `vsce` now supports configuration through the `package.json` file
    (see https://github.com/microsoft/vscode-vsce/issues/548), this option
    was dropped to simplify the codebase. One should either rely on the
    `vsce` automatically detecting when to use `yarn` (see
    https://github.com/microsoft/vscode-vsce/issues/480), or add the
    following to `package.json`:
    
    ```json
    {
        "vsce": {
            "yarn": true
        }
    }
    ```

--- test/publish.test.js
+++ test/publish.test.js
@@ -35,7 +35,7 @@ test('publish', async t => {
   sinon.stub(process, 'env').value({
     VSCE_PAT: token
   });
-  const result = await publish(version, undefined, undefined, logger);
+  const result = await publish(version, undefined, logger);
 
   t.deepEqual(result, {
     name: 'Visual Studio Marketplace',
@@ -64,42 +64,13 @@ test('publish with packagePath', async t => {
   sinon.stub(process, 'env').value({
     VSCE_PAT: token
   });
-  const result = await publish(version, packagePath, undefined, logger);
+  const result = await publish(version, packagePath, logger);
 
   t.deepEqual(result, {
     name: 'Visual Studio Marketplace',
     url: `https://marketplace.visualstudio.com/items?itemName=${publisher}.${name}`
   });
   t.deepEqual(execaStub.getCall(0).args, ['vsce', ['publish', '--packagePath', packagePath], { stdio: 'inherit' }]);
-});
-
-test('publish when yarn is true', async t => {
-  const { execaStub } = t.context.stubs;
-  const publisher = 'semantic-release-vsce';
-  const name = 'Semantice Release VSCE';
-  const publish = proxyquire('../lib/publish', {
-    execa: execaStub,
-    'fs-extra': {
-      readJson: sinon.stub().returns({
-        publisher,
-        name
-      })
-    }
-  });
-
-  const version = '1.0.0';
-  const token = 'abc123';
-  sinon.stub(process, 'env').value({
-    VSCE_PAT: token
-  });
-  const yarn = true;
-  const result = await publish(version, undefined, yarn, logger);
-
-  t.deepEqual(result, {
-    name: 'Visual Studio Marketplace',
-    url: `https://marketplace.visualstudio.com/items?itemName=${publisher}.${name}`
-  });
-  t.deepEqual(execaStub.getCall(0).args, ['vsce', ['publish', version, '--no-git-tag-version', '--yarn'], { stdio: 'inherit' }]);
 });
 
 test('publish with VSCE_PAT and VSCE_TOKEN should prefer VSCE_PAT', async t => {
@@ -122,7 +93,7 @@ test('publish with VSCE_PAT and VSCE_TOKEN should prefer VSCE_PAT', async t => {
     VSCE_PAT: token,
     VSCE_TOKEN: token
   });
-  const result = await publish(version, undefined, undefined, logger);
+  const result = await publish(version, undefined, logger);
 
   t.deepEqual(result, {
     name: 'Visual Studio Marketplace',
@@ -152,7 +123,7 @@ test('publish to OpenVSX', async t => {
     OVSX_PAT: token,
     VSCE_TOKEN: token
   });
-  const result = await publish(version, packagePath, undefined, logger);
+  const result = await publish(version, packagePath, logger);
 
   t.deepEqual(result, {
     name: 'Visual Studio Marketplace',
