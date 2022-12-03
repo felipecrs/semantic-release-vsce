2c96eaa63f251b38c0bee7558433a6ea7159790b
Felipe Santos <felipecassiors@gmail.com>
2022-12-03 11:51:19 -0300

    feat: verify ovsx personal access token (#371)

--- test/verify-ovsx-auth.test.js
+++ test/verify-ovsx-auth.test.js
@@ -1,6 +1,9 @@
 const test = require('ava');
 const sinon = require('sinon');
+const proxyquire = require('proxyquire');
 const SemanticReleaseError = require('@semantic-release/error');
+
+const cwd = process.cwd();
 
 test('OVSX_PAT is not set', async (t) => {
   const logger = {
@@ -22,7 +25,12 @@ test('OVSX_PAT is set', async (t) => {
     OVSX_PAT: 'abc123',
   });
 
-  const verifyOvsxAuth = require('../lib/verify-ovsx-auth');
+  const verifyOvsxAuth = proxyquire('../lib/verify-ovsx-auth', {
+    execa: sinon
+      .stub()
+      .withArgs('ovsx', ['verify-pat'], { preferLocal: true, cwd })
+      .resolves(),
+  });
 
   await t.notThrowsAsync(() => verifyOvsxAuth(logger));
   t.true(logger.log.calledOnce);
@@ -35,6 +43,23 @@ test('OVSX_PAT is invalid', async (t) => {
 
   sinon.stub(process, 'env').value({
     OVSX_PAT: '',
+  });
+
+  const verifyOvsxAuth = require('../lib/verify-ovsx-auth');
+
+  await t.throwsAsync(() => verifyOvsxAuth(logger), {
+    instanceOf: SemanticReleaseError,
+    code: 'EINVALIDOVSXPAT',
+  });
+});
+
+test('OVSX_PAT is invalid but not empty', async (t) => {
+  const logger = {
+    log: sinon.fake(),
+  };
+
+  sinon.stub(process, 'env').value({
+    OVSX_PAT: 'abc123',
   });
 
   const verifyOvsxAuth = require('../lib/verify-ovsx-auth');
